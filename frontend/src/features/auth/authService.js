ef2d8f88dda60a3638963e2ab20698c170ea5398
Wajud <wajudkareem757@gmail.com>
2024-10-10 15:47:19 +0100

    end of part 3

--- frontend/src/features/auth/authService.js
+++ frontend/src/features/auth/authService.js
@@ -5,15 +5,37 @@ const API_URL = "/api/users/";
 // Register user
 const register = async (userData) => {
   const response = axios.post(API_URL, userData);
-  if (response.data) {
-    localStorage.setItem("user", JSON.stringify(response.data));
+
+  const data = await (await response).data;
+
+  if (data) {
+    localStorage.setItem("user", JSON.stringify(data));
   }
 
-  return response.data;
+  return data;
+};
+
+const logout = () => {
+  localStorage.removeItem("user");
+};
+
+//Login user
+const login = async (userData) => {
+  const response = axios.post(API_URL + "login", userData);
+
+  const data = await (await response).data;
+
+  if (data) {
+    localStorage.setItem("user", JSON.stringify(data));
+  }
+
+  return data;
 };
 
 const authService = {
   register,
+  logout,
+  login,
 };
 
 export default authService;
