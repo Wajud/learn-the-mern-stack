200f9139558cd2e2b50968cd604ebee026d55a0c
Wajud <wajudkareem757@gmail.com>
2024-12-16 09:37:50 +0100

    prepared from deployment

--- backend/server.js
+++ backend/server.js
@@ -6,11 +6,12 @@ const { errorHandler } = require("./middleware/errorMiddleware");
 const connectDB = require("./config/db");
 const userRoutes = require("./routes/userRoutes");
 const cors = require("cors");
+const path = require("path");
+
 // const port = process.env.PORT || 5000;
 const port = 8000;
-
 connectDB();
-
+console.log("dirname here: ", __dirname);
 const app = express();
 
 // app.use(
@@ -23,6 +24,14 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use("/api/goals", goalRoutes);
 app.use("/api/users", userRoutes);
+
+//use the frontend app
+app.use(express.static(path.join(__dirname, "/frontend/build")));
+
+//Render client for any path
+app.get("*", (req, res) =>
+  res.sendFile(path.join(__dirname, "/frontend/build/index.html"))
+);
 
 app.use(errorHandler);
 
